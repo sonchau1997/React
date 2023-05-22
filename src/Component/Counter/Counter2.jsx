328e1b4ccdd623d1abd30f34c6b93d65bb1d4cf5
sonchau1997 <chauson23041997@gmail.com>
2023-05-22 16:20:09 +0700

    add

--- src/Component/Counter/Counter2.jsx
+++ src/Component/Counter/Counter2.jsx
@@ -1,7 +1,13 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export const Counter2=({name})=>{
     const [count,setCount]=useState(0);
+    const [loading,setLoading]=useState(false);
+    useEffect(()=>{
+        setTimeout(() => {
+          setLoading(true);},1000);
+
+    },[]);
     const onDecrease=()=>{
         setCount(count-1);
     };
@@ -10,12 +16,19 @@ export const Counter2=({name})=>{
     };
     return(
         <div>
-            <h1>Hello {name}</h1>
-            <button onClick={()=>onDecrease()}>-</button>
-            <span>{count}</span>
-            
-            <button onClick={()=>onIncrease()}  >+</button>
-        </div>
+                {
+                   !loading ?(
+                        <div>Loadingggggg</div> ):(
+
+                        <>
+                            <button onClick={() => onDecrease()}>-</button>
+                            <span>{count}</span>
+
+                            <button onClick={() => onIncrease()}  >+</button>
+                        </>)
+                }
+
+            </div>
     );
 
 };
